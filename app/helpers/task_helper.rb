4cde1bb20fe10f7360b2be6c9cab62da995a9ca5
Ruy Asan <ruy.asan@gmail.com>
2008-03-15 19:37:45 -0700

    Apropriate start/stop buttons

--- app/helpers/task_helper.rb
+++ app/helpers/task_helper.rb
@@ -3,10 +3,22 @@ module TaskHelper
     case (status = task.status)
     when :stopped:
       tag :input, :type=>"checkbox", :value=>status, :id=>dom_id(task, :status)
-    when :complete
+    when :complete:
       tag :input, :type=>"checkbox", :value=>status, :id=>dom_id(task, :status), :checked=>true
-    when :active
+    when :active:
       link_to image_tag("task_in_progress.png")
     end
   end
+  
+  def task_action_button(task)
+    case (status = task.status)
+    when :stopped:
+      link_to "Start", '#', :id=>dom_id(task, :start)
+    when :active:
+      link_to "Stop", '#', :id=>dom_id(task, :stop)
+    when :complete:
+      "&nbsp;"
+    end
+  end
+  
 end
