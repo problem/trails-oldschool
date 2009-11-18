e787b2a2498008526c575375a391f9412b79b46e
Dmitry Belitsky <Dmitry.Belitsky@gmail.com>
2009-11-18 06:56:35 +0200

    fix bug with tab on login and signup form.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,7 +3,7 @@
       'frameWidth': 690,
       'frameHeight': 650,
       'zoomSpeedIn': 300, 
-  		'zoomSpeedOut': 300
+  	  'zoomSpeedOut': 300
     });
     
     $("a:has(img)").fancybox({
@@ -27,28 +27,7 @@
       } 
     });
 
-    $('input:password').each(function(){
-      var $currentPass = $(this);
-      $currentPass.hide();
-      $currentPass.wrap('<div style="position: relative; height: 38px"></div>');
-      $currentPass.before('<input type="text" class="removeit ' + $currentPass.attr("id") + '" value="' + $currentPass.attr("rel") + '" style="position: absolute;" />');
-      
-      var $visiblePassword = $(".removeit");
-
-      $visiblePassword.focus(function () {
-        $(this).hide();
-        $currentPass.show();
-      });
-
-      $currentPass.blur( function () {
-        if ( $currentPass.attr("value") == "" ){
-          $currentPass.hide();
-          $visiblePassword.show();
-          }
-       });
-
-    });
-
+	$(".overlabel").overlabel();
 
   });
 
--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -57,28 +57,7 @@
       } 
     });
 
-
-    $('input:password').each(function(){
-      var $currentPass = $(this);
-      $currentPass.hide();
-      $currentPass.wrap('<div style="position: relative; height: 38px"></div>');
-      $currentPass.before('<input type="text" class="removeit ' + $currentPass.attr("id") + '" value="' + $currentPass.attr("rel") + '" style="position: absolute;" />');
-      
-      var $visiblePassword = $(".removeit");
-      $visiblePassword.removeClass("required");
-      $visiblePassword.focus(function () {
-        $(this).hide();
-        $currentPass.show();
-      });
-
-      $currentPass.blur( function () {
-        if ( $currentPass.attr("value") == "" ){
-          $currentPass.hide();
-          $visiblePassword.show();
-          }
-       });
-
-    });
+  	$(".overlabel").overlabel();
 
     $('#header h1 a').hover(
       function(){
